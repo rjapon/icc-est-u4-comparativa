0a39ed1c8aaa9520a1e5d1cd630314b097f05d87
rjapon <richardjv666@gmail.com>
2026-06-02 17:48:15 -0500

    Guardado

--- src/App.java
+++ src/App.java
@@ -7,40 +7,86 @@ import Utils.Benchmarking;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        int[] size = {10000,50000, 100000};
-        Persona[] arreglos = generarPersonas(size[0]);
-        Persona[] insertionCopy = arreglos.clone();
-        Persona[] quickCopy = arreglos.clone();
 
-        // SortPersonaMethods sortMethods = new SortPersonaMethods();
+        int[] size = { 10000, 50000, 100000 };
 
-        // Callable<Void> insertionSort = () -> {
-        //     sortMethods.insertionSort(insertionCopy);
-        //     return null;
-        // };
-    
-        // Resultado resultadosInsertion = Benchmarking.medirTiempo(insertionSort, "Insertion Sort", "Personas", size[0]);
-        // System.out.println(resultadosInsertion.getAlgoritmo() + ": " + resultadosInsertion.getTiempoMillis() + " ms");
-
-        // Callable<Void> quickSort = () -> {
-        //     sortMethods.quickSort(quickCopy, 0, quickCopy.length - 1);
-        //     return null;
-        // };
-
-        // Resultado resultadosQuick = Benchmarking.medirTiempo(quickSort, "Quick Sort", "Personas", size[0]);
-        // System.out.println(resultadosQuick.getAlgoritmo() + ": " + resultadosQuick.getTiempoMillis() + " ms");
+        for (int s : size) {
+            ejecutarEscenarioDesordenado(s);
+            ejecutarEscenarioOrdenado(s);
+        }
 
     }
 
-    public static void ejecutarEscenarioDesordenado(){
-        // Implementar el escenario desordenado
+    // Escenario 1: Personas desordenadas
+    public static void ejecutarEscenarioDesordenado(int size) {
+
+        Persona[] base = generarPersonas(size);
+        Persona[] insertionCopy = base.clone();
+        Persona[] quickCopy = base.clone();
+
+        SortPersonaMethods sortMethods = new SortPersonaMethods();
+
+        Callable<Void> insertionSort = () -> {
+            sortMethods.insertionSort(insertionCopy);
+            return null;
+        };
+        Resultado resultadosInsertion = Benchmarking.medirTiempo(insertionSort, "Insertion Sort", "Personas", size);
+        System.out.println(
+                "\nDesordenado | " + resultadosInsertion.getAlgoritmo() + " | " + resultadosInsertion.getSample() + " | " + resultadosInsertion.getTiempoMillis() + " ms");
+
+        Callable<Void> quickSort = () -> {
+            sortMethods.quickSort(quickCopy, 0, quickCopy.length - 1);
+            return null;
+        };
+        Resultado resultadosQuick = Benchmarking.medirTiempo(quickSort, "Quick Sort", "Personas", size);
+        System.out.println(
+                "Desordenado | " + resultadosQuick.getAlgoritmo() + " | " + resultadosQuick.getSample() + " | " + resultadosQuick.getTiempoMillis() + " ms");
+
     }
-    
-    public static Persona[] generarPersonas(int cantidad){
+
+    public static void ejecutarEscenarioOrdenado(int size) {
+
+        Persona[] base = generarPersonas(size);
+
+        SortPersonaMethods sortMethods = new SortPersonaMethods();
+
+        sortMethods.quickSort(base, 0, base.length - 1);
+
+        Persona[] nuevoArreglo = new Persona[base.length + 1];
+
+        for (int i = 0; i < base.length; i++) {
+            nuevoArreglo[i] = base[i];
+        }
+
+        nuevoArreglo[nuevoArreglo.length - 1] = new Persona("NuevaPersona", 100);
+
+        Persona[] insertionCopy = nuevoArreglo.clone();
+        Persona[] quickCopy = nuevoArreglo.clone();
+
+        Callable<Void> insertionSort = () -> {
+            sortMethods.insertionSort(insertionCopy);
+            return null;
+        };
+
+        Resultado resultadosInsertion = Benchmarking.medirTiempo(insertionSort, "Insertion Sort", "Personas",
+                nuevoArreglo.length);
+
+        System.out.println("Casi Ordenado + 1 persona | " + resultadosInsertion.getAlgoritmo() + " | " + resultadosInsertion.getSample() + " | " + resultadosInsertion.getTiempoMillis() + " ms");
+
+        Callable<Void> quickSort = () -> {
+            sortMethods.quickSort(quickCopy, 0, quickCopy.length - 1);
+            return null;
+        };
+
+        Resultado resultadosQuick = Benchmarking.medirTiempo(quickSort, "Quick Sort", "Personas", nuevoArreglo.length);
+
+        System.out.println("Casi Ordenado + 1 persona | " + resultadosQuick.getAlgoritmo() + " | " + resultadosQuick.getSample() + " | " + resultadosQuick.getTiempoMillis() + " ms");
+    }
+
+    public static Persona[] generarPersonas(int cantidad) {
         Persona[] personas = new Persona[cantidad];
 
-        for(int i = 0; i < cantidad; i++){
+        for (int i = 0; i < cantidad; i++) {
             String nombre = "Persona" + (i + 1);
             int edad = (int) (Math.random() * 101);
             personas[i] = new Persona(nombre, edad);
--- src/Controllers/SortPersonaMethods.java
+++ src/Controllers/SortPersonaMethods.java
@@ -4,21 +4,21 @@ import Models.Persona;
 
 public class SortPersonaMethods {
 
-    public void insertionSort(Persona[] personas){
+    public void insertionSort(Persona[] personas) {
 
-        for (int i = 0; i < personas.length; i++) {
+        for (int i = 1; i < personas.length; i++) {
             Persona aux = personas[i];
             int j = i - 1;
-            while (j >= 0 && personas[j].getCriterioOrdenamiento > aux.getCriterioOrdenamiento) {
-                personas[j+1] = personas[j];
+            while (j >= 0 && personas[j].getCriterioOrdenamiento() > aux.getCriterioOrdenamiento()) {
+                personas[j + 1] = personas[j];
                 j--;
             }
-            personas[j+1] = aux;
+            personas[j + 1] = aux;
         }
 
     }
 
-    public void quickSort(Persona[] personas, int inicio, int fin){
+    public void quickSort(Persona[] personas, int inicio, int fin) {
         if (inicio < fin) {
             int indicePivote = particionar(personas, inicio, fin);
             quickSort(personas, inicio, indicePivote - 1);
@@ -26,21 +26,27 @@ public class SortPersonaMethods {
         }
     }
 
-    private int particionar(Persona[] personas, int inicio, int fin){
+    private int particionar(Persona[] personas, int inicio, int fin) {
+
+        int medio = (inicio + fin) / 2;
+        intercambiar(personas, medio, fin);
+
         Persona pivote = personas[fin];
         int i = inicio - 1;
 
-        for(int j = inicio; j < fin; j++){
-            if(personas[j].getCriterioOrdenamiento <= pivote.getCriterioOrdenamiento){
+        for (int j = inicio; j < fin; j++) {
+            if (personas[j].getCriterioOrdenamiento() <= pivote.getCriterioOrdenamiento()) {
+
                 i++;
                 intercambiar(personas, i, j);
             }
         }
+
         intercambiar(personas, i + 1, fin);
         return i + 1;
     }
 
-    private void intercambiar(Persona[] personas, int i, int j){
+    private void intercambiar(Persona[] personas, int i, int j) {
         Persona aux = personas[i];
         personas[i] = personas[j];
         personas[j] = aux;
